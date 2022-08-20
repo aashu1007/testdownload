b8d44d0f9473227ff2319d90f8cdc76f7d19625c
aashu1007 <aashu.kushwaha1007@gmail.com>
2022-08-20 20:42:55 +0530

    comitting changes on local

---
+++ MVCmicroserviceDocker/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MVCmicroserviceDocker.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MVCmicroserviceDocker/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MVCmicroserviceDocker.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
